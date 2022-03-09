25352d305c8cb32cef5c7b53e507309a838eff7a
Vivek Kumar Singh <vivekhappy786@gmail.com>
2022-03-09 12:34:38 +0530

    initialized backend and frontend

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+app.get("/api/notes/", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.json(note);
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log("Server on 5000"));
